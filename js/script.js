aa64e8a61571cff2d2128b5e22cc319d70c137c8
RagedHavoc <annie.johnson1we@gmail.com>
2019-11-20 22:17:47 +0000

    HEY BOB YOUR DONE RELAX

--- js/script.js
+++ js/script.js
@@ -9,42 +9,45 @@ $("#shoot").click(function(){
     let userChoice = $("input").val();
     let computerChoice;
     let randomNumber = Math.random();
-
+    userChoice = userChoice.toLowerCase();
+    
     if(randomNumber > 0 && randomNumber < .4){
         computerChoice = "rock"
     }
     if(randomNumber > .4 && randomNumber < .7){
         computerChoice = "paper"
     }
-    if(randomNumber > 0 && randomNumber < .3){
+    if(randomNumber > .7 && randomNumber < 1){
         computerChoice = "scissors"
-    }
-)};
+    };
 
+    $("#userChoice").html(userChoice);
+    $("#computerChoice").html(computerChoice);
 
-    if(userChoice = "rock" && computerChoice == "rock")
+    if(userChoice == "rock" && computerChoice == "rock"){
         $("#text").html("It's a tie!");
-    else if(userChoice = "paper" && computerChoice == "paper")
+    }else if(userChoice == "paper" && computerChoice == "paper"){
         $("#text").html("It's a tie!");
-    else if(userChoice = "scissors" && computerChoice == "scissors")
+    }else if(userChoice == "scissors" && computerChoice == "scissors"){
         $("#text").html("It's a tie!");
 
-    else if(userChoice = "rock" && computerChoice == "paper")
+    }else if(userChoice == "rock" && computerChoice == "paper"){
         $("#text").html("Computer Wins!");
-    else if(userChoice = "rock" && computerChoice == "scissors")
+    }else if(userChoice == "rock" && computerChoice == "scissors"){
         $("#text").html("You Win!");
     
-    else if(userChoice = "paper" && computerChoice == "scissors")
-        $(#text).html("Computer Wins!");
-    else if(userChoice = "paper" && computerChoice == "rock")
-        $("#text").html("You Win!");
-
-    else if(userChoice = "scissors" && computerChoice == "rock")
+    }else if(userChoice == "paper" && computerChoice == "scissors"){
         $("#text").html("Computer Wins!");
-    if(userChoice = "scissors" && computerChoice == "paper")
+    }else if(userChoice == "paper" && computerChoice == "rock"){
         $("#text").html("You Win!");
 
-    } else $("#text").html("Thats not a valid input");
-    }
+    }else if(userChoice == "scissors" && computerChoice == "rock")
+        $("#text").html("Computer Wins!");
+    else if(userChoice == "scissors" && computerChoice == "paper"){
+        $("#text").html("You Win!");
+
+} else{ $("#text").html("Thats not a valid input");
+}
+});
 // DOCUMENT READY FUNCTION BELOW
 
